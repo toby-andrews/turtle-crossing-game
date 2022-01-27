8108238beb263ab2c370f322a3a5038dde0133b3
Toby Karagas-Andrews <94924045+toby-andrews@users.noreply.github.com>
2022-01-27 10:02:42 +0300

    Add files via upload
    updated car manager to increase chance of car being generated as game speed up as they are not on the screen for as long. If using may need to edit the import in the main file

---
+++ car_manager_V2.py
@@ -0,0 +1,37 @@
+from turtle import Turtle
+import random
+
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 2
+
+
+class CarManager:
+
+    def __init__(self):
+        self.all_cars = []
+
+    def create_car(self):
+        global STARTING_MOVE_DISTANCE
+        if STARTING_MOVE_DISTANCE > 20:
+            random_chance = random.randint(1, 3)
+        elif STARTING_MOVE_DISTANCE > 13:
+            random_chance = random.randint(1, 4)
+        else:
+            random_chance = random.randint(1, 5)
+        if random_chance == 1:
+            new_car = Turtle("square")
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            starting_y_coord = random.randint(-245, 245)
+            new_car.goto(300, starting_y_coord)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
+
+    def level_up(self):
+        global STARTING_MOVE_DISTANCE
+        STARTING_MOVE_DISTANCE += MOVE_INCREMENT
